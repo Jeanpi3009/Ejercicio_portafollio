8809b39112738fb12f55083ac940bd13409a9474
jeanpierre <jeanpierreloaizaleon@gmail.com>
2023-02-28 20:56:48 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Cards from './components/Cards/Cards'
+import Discount from './components/Discount/Discount'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+import Review from './components/Review/Review'
+import Staff from './components/Staff/Staff'
+import Tours from './components/Tours/Tours'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      {/* <Home/>
+      <Cards/>
+      <Tours/>
+      <Discount/>
+      <Review/>
+      <Staff/>
+      <Footer/> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './Navbar.css'
+
+//import icons
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {BsFacebook} from 'react-icons/bs'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiFillYoutube} from 'react-icons/ai'
+import {AiFillInstagram} from 'react-icons/ai'
+import {TbGridDots} from 'react-icons/tb'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="header flex">
+        <div className="logoDiv">
+          <h3 className="logo">Wonder</h3>
+        </div>
+        <div className="menuDiv">
+          <ul className="menuLists">
+            <li className="navItem">
+              <a href="#" className="menuLink">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLink">About</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLink">Our tours</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLink">Gallery</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLink">Blog</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLink">Contact Us</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLink">Pages</a>
+            </li>
+          </ul>
+
+          <div className="closeNavbar">
+            <AiFillCloseCircle className='icon'/>
+          </div>
+        </div>
+
+        <div className="socialIcon flex">
+          <BsFacebook className='icon'/>
+          <AiOutlineTwitter className='icon'/>
+          <AiFillYoutube className='icon'/>
+          <AiFillInstagram className='icon'/>
+        </div>
+
+        <div className="toggleNavbar">
+          <TbGridDots className='icon'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
